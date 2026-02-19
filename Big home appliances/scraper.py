0d9754d8c444fb3cca5de1828d62195f8392ce8c
Nikolay Nikolov <97630031+krufeee@users.noreply.github.com>
2026-02-19 11:22:14 +0200

    creating modified scraper for custom group

--- Big home appliances/scraper.py
+++ Big home appliances/scraper.py
@@ -1,11 +1,11 @@
 
 from playwright.sync_api import sync_playwright
 import pandas as pd
-
+from pathlib import Path
 
 def run():
     with sync_playwright() as p:
-        # Starting chromium browser
+        # Starting Chromium browser
         browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
@@ -38,20 +38,45 @@ def run():
 
                 return data
 
+            def get_product_category(curr_product):
+                product_category_list = curr_product.get("categories")
+                product_category = product_category_list[0]
+                c_name = product_category.get("name")
+                return c_name
+
 
             # Function for extracting product details for current product
             def extract_product_data(current_product):
+                gifts_list = current_product.get("potentialPromotions", "")
+                current_gift = ''
+                if gifts_list:
+                    free_gift = gifts_list[0]
+                    free_gift_list = free_gift.get("freeGifts")
+                    free_gift_final = free_gift_list[0]
+                    current_gift = free_gift_final.get("description")
+
                 return {
                     "name": current_product.get("name"),
                     "price": float(current_product.get("price", {}).get("value")),
-                    "link": "https://www.technopolis.bg" + current_product.get("url", ""),
+                    "code": current_product.get("code"),
+                    "gift": current_gift
 
                 }
             # Function for exporting extracted products details in excel sheet
-            def export_to_excel(list_of_products, period):
-                df = pd.DataFrame(list_of_products)
-                df.to_excel(f"Technopolis promotion {period}.xlsx", index=False)
+            def export_to_excel_in_promotion_folder(list_of_products, period):
+                # Get the directory where the script is located
+                script_dir = Path(__file__).parent
+                # Define output folder path
+                output_folder = script_dir / 'Promotions'
+                # Create output folder if it doesn't exist
+                output_folder.mkdir(exist_ok=True)
+
+                columns_order = ['code', 'name', 'price', 'gift']
+                df = pd.DataFrame(list_of_products, columns=columns_order)
+                df.to_excel(output_folder / f"Big home appliances promotions {period}.xlsx", index=False)
                 print('Exported to excel...')
+                output_file = output_folder / f"Big home appliances promotions {period}.xlsx"
+                print(f"File saved to: {output_file}")
 #------------------------------------------------------------------------------------------------------
 
             # Чакаме само зареждане на DOM, за да не увисва на реклами
@@ -78,11 +103,8 @@ def run():
             try:
                 page.wait_for_selector(selector, timeout=2000)
                 print("Ad popup.")
-                page.wait_for_timeout(2000)
                 page.wait_for_selector(button, timeout=2000)
-                page.wait_for_timeout(2000)
                 page.click(button)
-                page.wait_for_timeout(2000)
                 print('Ad closed.')
 
             except:
@@ -100,18 +122,36 @@ def run():
             print(f'Found {number_of_pages} pages.')
             print(f'Found {number_of_products} products.')
 
+            hardcoded_categories = ("Климатици,Мобилни климатици,Термопомпи,Перални с предно зареждане,"
+                                    "Перални с горно зареждане,Перални със сушилни,Перални за вграждане,"
+                                    "Перални със сушилни за вграждане,Сушилни,Хладилници с долен фризер,"
+                                    "Хладилници с горен фризер,Хладилници Side by Side,Хладилници с една врата,"
+                                    "Минибар,Хладилници за вграждане,Виноохладители,Хладилни витрини,"
+                                    "Вертикални фризери,Хоризонтални фризери тип ракла,Фризери за вграждане,"
+                                    "Свободностоящи съдомиялни,Компактни съдомиялни,Съдомиялни за вграждане,"
+                                    "Електрически готварски печки,Газови готварски печки,Комбинирани готварски печки,"
+                                    "Фурни за вграждане,Плотове за вграждане,Обемни бойлери,Проточни бойлери,"
+                                    "Телескопични абсорбатори,Абсорбатори за стенен монтаж,Абсорбатори за пълно вграждане"
+                                    "Островен тип абсорбатори,Микровълнови за вграждане")
+
             clean_products = []
+
 
             for current_page in range(1,number_of_pages + 1):
                     current_data = get_page_number_return_data(current_page)
                     products = current_data["products"]
-                    print(f'Extracting {current_page}.')
+                    print(f'Extracting page number {current_page}.')
 
                     for product in products:
-                        extracted = extract_product_data(product)
-                        clean_products.append(extracted)
+                        category_name = get_product_category(product)
+                        if category_name in hardcoded_categories:
+                            extracted = extract_product_data(product)
+                            clean_products.append(extracted)
+                        else:
+                            continue
 
-                    print(f'Extracted {len(clean_products)} products.')
+                    if clean_products:
+                        print(f'Extracted {len(clean_products)} products.')
 
 
             print(f'Removing duplicating products.')
@@ -120,13 +160,16 @@ def run():
 
             # Convert back to dictionaries
             unique_products = [dict(p) for p in unique_products]
-            unique_products = sorted(unique_products, key=lambda x: x["price"])
+            unique_products = sorted(unique_products, key=lambda x: x["name"])
 
             promotion_period_element = page.query_selector('.content-title')
             promotion_period = promotion_period_element.inner_text()
             print(f'Extracted {len(unique_products)} unique products.')
 
-            export_to_excel(unique_products, promotion_period.split()[0])
+            export_to_excel_in_promotion_folder(unique_products, promotion_period.split()[0])
+
+
+
 
 
         except Exception as e:
